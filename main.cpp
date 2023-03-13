f838ab1077445fa4dbcd52d68e2223acc1bc90e7
Michael Bethke <gibsonbethke@gmail.com>
2023-03-12 21:46:22 -0500

    minor improvements to SDL window demo

--- main.cpp
+++ main.cpp
@@ -5,18 +5,15 @@
 
 
 SDL_Window* SetupSDL() {
-	SDL_Window* window = nullptr;
-	SDL_Surface* screen = nullptr;
-
 	if(SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL initialization error!" << std::endl;
 	} else {
-		window = SDL_CreateWindow("Test Window Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
-		if(!window) {
+		SDL_Window* window = SDL_CreateWindow("Test Window Title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN);
+		if(window == nullptr) {
 			std::cout << "Failed creating window!" << std::endl;
 		} else {
-			screen = SDL_GetWindowSurface(window);
-			SDL_FillRect(screen, nullptr, SDL_MapRGB(screen->format, 69, 69, 69));
+			SDL_Surface& screen = *SDL_GetWindowSurface(window);
+			SDL_FillRect(&screen, nullptr, SDL_MapRGB(screen.format, 69, 69, 69));
 			SDL_UpdateWindowSurface(window);
 
 			return window;
@@ -38,10 +35,10 @@ int main(int argc, char* argv[]) {
 					quit = true;
 			}
 		}
+		
+		SDL_DestroyWindow(window);
 	}
 
-	SDL_DestroyWindow(window);
 	SDL_Quit();
-
 	return 0;
 }

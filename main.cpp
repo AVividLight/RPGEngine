7ed935dc39406155f5b90e538a892d8aa59e122f
Michael Bethke <gibsonbethke@gmail.com>
2023-08-16 21:15:54 -0500

    trying to work out chrono frame time

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
 #include "RenderingEngine.hpp"
 #include "KeyboardInput.hpp"
 #include <SDL.h>
+#include <chrono>
+
+
+using TARGET_FRAME_RATE = std::ratio<1, 30>;
 
 
 int main(int argc, char* argv[]) {
 	RenderingEngine renderer("RPGEngine");
 	KeyboardInput input;
+	std::chrono::steady_clock time;
+	std::chrono::time_point<std::chrono::steady_clock> lastFrameTimestamp{};
+	std::chrono::time_point<std::chrono::steady_clock> currentFrameTimestamp{};
+	std::chrono::duration<long long, std::nano> frameDelta;
 
 	input.ToggleKeyboardInput(true);
 
 	SDL_Event e;
 	bool quit = false;
 	do {
+		currentFrameTimestamp = std::chrono::steady_clock::now();
+		frameDelta = currentFrameTimestamp - lastFrameTimestamp;
+		
+
+
 		while(SDL_PollEvent(&e)) {
 			switch(e.type)
 			{

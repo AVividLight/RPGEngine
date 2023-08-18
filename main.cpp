ddf686a31dfe94edf0d259a8e7816dc262089f63
Michael Bethke <gibsonbethke@gmail.com>
2023-08-18 13:08:51 -0500

    Updating code from demo & separated art source and output

--- main.cpp
+++ main.cpp
@@ -1,8 +1,46 @@
+#include "RenderingEngine.hpp"
+#include "KeyboardInput.hpp"
 #include <SDL.h>
+#include <chrono>
+
+
+using TARGET_FRAME_RATE = std::ratio<1, 30>;
 
 
 int main(int argc, char* argv[]) {
-	
+	RenderingEngine renderer("RPGEngine");
+	KeyboardInput input;
+	std::chrono::steady_clock time;
+	std::chrono::time_point<std::chrono::steady_clock> lastFrameTimestamp{};
+	std::chrono::time_point<std::chrono::steady_clock> currentFrameTimestamp{};
+	std::chrono::duration<long long, std::nano> frameDelta;
+
+	input.ToggleKeyboardInput(true);
+
+	SDL_Event e;
+	bool quit = false;
+	do {
+		currentFrameTimestamp = std::chrono::steady_clock::now();
+		frameDelta = currentFrameTimestamp - lastFrameTimestamp;
+		while(frameDelta < std::chrono::duration<long long, TARGET_FRAME_RATE>{1}) {
+			if(SDL_PollEvent(&e)) {
+				switch(e.type)
+				{
+				case SDL_QUIT:
+					quit = true;
+				break;
+
+				case SDL_TEXTINPUT:
+					renderer.UpdateMainText(e.text.text);
+				break;
+				}
+			}
+
+			renderer.Repaint();
+		}
+	} while(quit == false);
+
+	input.ToggleKeyboardInput(false);
 
 	return 0;
 }
